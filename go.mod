f5d0d30a991627ac7e80bdf1965d8471d79be31b
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2025-10-14 02:04:33 +0000

    更新了版本号

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module sfs
+module fileserver
 
-go 1.24.1
+go 1.25.3
--- main.go
+++ main.go
@@ -11,44 +11,24 @@ import (
 	"strings"
 )
 
-var rootfs *os.Root
-
-type info struct {
-	name string
-	size string
-	time string
-	i    int
-	j    int
+type fileHandler struct {
+	root *os.Root
 }
 
-func main() {
-	port := flag.String("port", "8080", "server port")
-	root := flag.String("path", "./", "server root path")
-	flag.Parse()
-
-	*root, _ = filepath.Abs(*root)
-	rootfs, _ = os.OpenRoot(*root)
-
-	fmt.Printf("@ 0.0.0.0:%s\n@ %s\n", *port, *root)
-	http.HandleFunc("/", server)
-	http.ListenAndServe(":"+*port, nil)
-}
-
-func server(w http.ResponseWriter, req *http.Request) {
+func (fh *fileHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("%s\n", req.URL.Path)
 
-	p := strings.TrimPrefix(req.URL.Path, "/")
-	if p == "" {
-		p = "."
+	path := strings.TrimPrefix(req.URL.Path, "/")
+	if path == "" {
+		path = "."
 	}
 
-	fi, err := rootfs.Stat(p)
+	fi, err := fh.root.Stat(path)
 	if err != nil {
 		http.NotFound(w, req)
 		return
 	}
-
-	f, _ := rootfs.Open(p)
+	f, _ := fh.root.Open(path)
 	defer f.Close()
 
 	if !fi.IsDir() {
@@ -56,9 +36,8 @@ func server(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	fd, _ := f.ReadDir(-1)
-
-	slices.SortStableFunc(fd, func(a, b os.DirEntry) int {
+	entries, _ := f.ReadDir(-1)
+	slices.SortStableFunc(entries, func(a, b os.DirEntry) int {
 		if a.IsDir() != b.IsDir() {
 			if a.IsDir() {
 				return -1
@@ -70,33 +49,53 @@ func server(w http.ResponseWriter, req *http.Request) {
 
 	var b strings.Builder
 
+	padding := make([]byte, 51)
+	for i := range padding {
+		padding[i] = ' '
+	}
+
 	of := "Index of " + req.URL.Path
 	b.WriteString(fmt.Sprintf("<title>%s</title><h1>%s</h1>", of, of))
 	b.WriteString("<hr><pre><a href=\"../\">../</a>\n")
 
-	var i info
-	for _, d := range fd {
+	for _, d := range entries {
+		var i, j int
+		var name, size, time string
+
 		l, _ := d.Info()
-
 		if l.IsDir() {
-			i.name = l.Name() + "/"
-			i.size = "-"
+			name = l.Name() + "/"
+			size = "-"
 		} else {
-			i.name = l.Name()
-			i.size = strconv.FormatInt(l.Size(), 10)
+			name = l.Name()
+			size = strconv.FormatInt(l.Size(), 10)
 		}
+		time = l.ModTime().Format("02-Jan-2006 15:04")
 
-		i.time = l.ModTime().Format("02-Jan-2006 15:04")
-		i.i = max(51-len(i.name), 1)
-		i.j = max(20-len(i.size), 1)
+		i = max(51-len(name), 1)
+		j = max(20-len(size), 1)
 
-		b.WriteString(fmt.Sprintf("<a href=\"%s\">%s</a>", i.name, i.name))
-		b.WriteString(strings.Repeat(" ", i.i))
-		b.WriteString(i.time)
-		b.WriteString(strings.Repeat(" ", i.j))
-		b.WriteString(i.size)
+		b.WriteString(fmt.Sprintf("<a href=\"%s\">%s</a>", name, name))
+		b.Write(padding[:i])
+		b.WriteString(time)
+		b.Write(padding[:j])
+		b.WriteString(size)
 		b.WriteString("\n")
 	}
 	b.WriteString("</pre><hr>")
-	w.Write([]byte(b.String()))
+	b.WriteTo(w)
+}
+
+func main() {
+	port := flag.String("port", "8080", "server port")
+	path := flag.String("path", "./", "server path")
+	flag.Parse()
+
+	fmt.Printf("@ %s, @ 0.0.0.0:%s\n", *path, *port)
+
+	*path, _ = filepath.Abs(*path)
+	rootfs, _ := os.OpenRoot(*path)
+
+	http.Handle("/", &fileHandler{root: rootfs})
+	http.ListenAndServe(":"+*port, nil)
 }
